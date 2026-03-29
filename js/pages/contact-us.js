6500a923e0f437c360d11c4ddfc10590c7778e8a
Rhaastrake <garofalomichele2001@gmail.com>
2026-03-29 18:17:25 +0200

    Aggiunto mobile-menu (solo logica)

--- js/pages/contact-us.js
+++ js/pages/contact-us.js
@@ -1,11 +1,13 @@
 import { initMain } from '../modules/main.js';
 import { initScrollHeader } from '../modules/scrollHeader.js';
+import { initBurgerMenu } from '../modules/burgerMenu.js';
 import { initTextAreaAutoExpand } from '../modules/textAreaAutoExpand.js';
 
 //Tutto ciò che ha bisogno del domcontentloaded, observer ecc deve avere un init ed essere fatto partire qui
 document.addEventListener("DOMContentLoaded", () => {
     initScrollHeader()
     initTextAreaAutoExpand()
+    initBurgerMenu();
 })
 
 
