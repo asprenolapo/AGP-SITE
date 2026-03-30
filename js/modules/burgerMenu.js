dd6dd38e991fedfbc4fa635ee3621ad90c3f9471
Michele <garofalomichele2001@gmail.com>
2026-03-30 10:23:27 +0200

    Aggunto mobile-menu funzionante

--- js/modules/burgerMenu.js
+++ js/modules/burgerMenu.js
@@ -1,11 +1,13 @@
 export function initBurgerMenu() {
-    const header = document.querySelector("header");
     const burgerButton = document.querySelector(".burger-button");
     const menuBurgerButton = document.querySelector(".menu-burger-button");
     const mobileMenu = document.querySelector(".mobile-menu");
 
     burgerButton.addEventListener("click", () => {
         mobileMenu.style.display = "flex";
-        header.classList.add("hidden");
+    })
+
+    menuBurgerButton.addEventListener("click", () => {
+        mobileMenu.style.display = "none";
     })
 }
